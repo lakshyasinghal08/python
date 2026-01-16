24f938732ab56fce67b1d2cee5ad9aa25fab6933
Lakshya Singhal <lakshyasinghal92@gmail.com>
2026-01-16 16:16:27 +0530

    loops and functions

---
+++ loops.py
@@ -0,0 +1,47 @@
+# for i in range(10):
+#     print(i)
+    
+# for i in range(1,20):
+#     print(i)
+    
+# num = [1,3,4,5,6,7,8,9,10]
+
+# for x in num:
+#     print(x)
+
+# for ch in "python":
+#     print(ch)
+
+# i = 1
+# while i<=10:
+#     print(i)
+#     i+=1
+    
+# for i in range(1,10):
+#     if i == 5:
+#         break
+#     print(i)
+    
+# for a in range(1,10):
+#     if a == 5:
+#        continue
+#     print(a)
+   
+# for i in range(1,20,2):
+#     print(i)
+    
+# for a in range(3):
+#     print(a)
+    
+# for x in range(1,11):
+#     print(x)
+    
+# i = 5
+# while i>=1:
+#     print(i)
+#     i -= 1
+
+for i in range(1,11):
+    if(i % 2 == 0):
+        print(i)
+        
